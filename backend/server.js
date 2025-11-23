54288d5a6c2a916e7b3547d6e7aec1b58503cbf8
Udit-Jangid <uditjangid20012015@gmail.com>
2025-11-23 15:50:48 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
@@ -21,13 +22,25 @@ const app = express();
 
 app.use(express.json()); // to accept JSON Data
 
-app.get("/", (req, res) => {
-  res.send("API is Running");
-});
-
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+//------------ Deployment --------------
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/dist")));
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is Running");
+  });
+}
+
+//------------ Deployment --------------
 
 // error and not found request handler
 
